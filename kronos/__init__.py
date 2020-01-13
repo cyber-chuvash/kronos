c7218a656ac5d6cfd46278d6fbf422ee6bafd888
Mikhail Migushov <me@chuvash.pw>
2020-01-14 02:13:50 +0300

    Basic working Proof of Concept

---
+++ kronos/__init__.py
@@ -0,0 +1,28 @@
+import re
+
+
+_time_intervals = {
+    r'sec': 1,
+    r'min': 60,
+    r'h': 3600,
+    r'd': 86400,
+    r'M': 2592000,
+    r'y': 31536000
+}
+
+_time_int_regex = {}
+
+for _name, _interval in _time_intervals.items():
+    _time_int_regex[re.compile(r'(\d)\s+' + _name)] = _interval
+
+
+def parse(string):
+    result_seconds = 0
+
+    for regex, interval in _time_int_regex.items():
+        for match in regex.finditer(string):
+            num = int(match.group(1))
+            result_seconds += num * interval
+
+    return result_seconds
+
